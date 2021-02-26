fd68f3e7ce2669ad8650acf843d14d8aa98b90df
Alexander Hurtado Cardona <ahurtadoc@unal.edu.co>
2021-02-26 13:59:09 -0500

    Aplicación simple de inventario php-angular

---
+++ Controllers/InventarioController.php
@@ -0,0 +1,54 @@
+<?php
+
+
+namespace Gestor\Controller;
+
+
+class InventarioController extends Controller
+{
+    public function read()
+    {
+        $productos = $this->db->findAll('productos');
+        return $this->toJson($productos);
+    }
+    
+    public function create($params)
+    {
+        $datos = $this->fromJson($params);
+        $datos['fechaCreacion'] = date('Y-m-d');
+        $response = $this->db->insert('productos', $datos);
+        return $this->toJson($response);
+    }
+
+    public function update($params)
+    {
+        $datos = $this->fromJson($params);
+        $cambios = $datos['cambios'];
+        $id = $datos['id'];
+        $response = $this->db->update('productos', $cambios, $id);
+        return $this->toJson($response);
+    }
+
+    public function delete($params)
+    {
+        $id = $this->fromJson($params);
+        $response = $this->db->delete('productos', $id);
+        return $this->toJson($response);
+    }
+
+    public function sell($params)
+    {
+        $id = $this->fromJson($params);
+        $stock = $this->db->find('productos',$id,array("stock"));
+        $stock = $stock[0]["stock"];
+        if ($stock <= 0){
+            return $this->toJson(array('No hay stock de este producto'));
+        }else{
+            $cambios = array("stock" => $stock-1);
+            if($this->db->update('productos',$cambios,$id)){
+                return $this->toJson(array('producto vendido '));
+            }
+        }
+    }
+
+}
---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+
+require_once 'vendor/autoload.php';
+date_default_timezone_set('America/Bogota');
+
+use Gestor\Controller\InventarioController;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+use Slim\Routing\RouteCollectorProxy;
+
+
+$app = AppFactory::create();
+$app->setBasePath('/inventario');
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello world!");
+    return $response;
+});
+$app->group('/productos', function (RouteCollectorProxy $group) {
+    $ic = new InventarioController();
+    $group->get('', function ($request, $response, array $args) use ($ic) {
+        $payload = $ic->read();
+        $response->getBody()->write($payload);
+        return $response
+            ->withHeader('Content-Type', 'application/json');
+
+    });
+
+    $group->post('', function ($request, $response, array $args) use ($ic) {
+        $body = $request->getBody();
+        $payload = $ic->create($body);
+        $response->getBody()->write($payload);
+        return $response
+            ->withHeader('Content-Type', 'application/json');
+    });
+
+    $group->put('', function ($request, $response, array $args) use ($ic) {
+        $body = $request->getBody();
+        $payload = $ic->create($body);
+        $response->getBody()->write($payload);
+        return $response
+            ->withHeader('Content-Type', 'application/json');
+    });
+
+    $group->delete('', function ($request, $response, array $args) use ($ic) {
+        $body = $request->getBody();
+        $payload = $ic->delete($body);
+        $response->getBody()->write($payload);
+        return $response
+            ->withHeader('Content-Type', 'application/json');
+    });
+
+    $group->post('/vender', function ($request, $response, array $args) use ($ic) {
+        $body = $request->getBody();
+        $payload = $ic->sell($body);
+        $response->getBody()->write($payload);
+        return $response
+            ->withHeader('Content-Type', 'application/json');
+    });
+});
+
+$app->run();
